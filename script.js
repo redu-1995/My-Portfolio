27683efe270079f7ad6d46cbfcd717f6bab465bf
redu-1995 <nara1415karol@gmail.com>
2025-04-20 12:16:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById('menu-toggle')
+const navLinks = document.querySelector('.nav-links')
+toggle.addEventListener('click', () =>{
+    navLinks.classList.toggle('show');
+})
