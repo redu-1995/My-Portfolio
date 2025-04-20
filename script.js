f4b44285515f8e840b7f3600cc44ce502aa540f8
redu-1995 <nara1415karol@gmail.com>
2025-04-20 12:37:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  });
+  
